90fcf3e22d45b466386f672998c26cd0ad71b627
sshmatrix <cryptosauron@protonmail.com>
2025-01-06 15:38:59 +0530

    rSync

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -427,7 +427,7 @@ export const shortenTick = (tick, useBinning) => {
 export const copyText = debounce(async (text) => {
   try {
     await navigator.clipboard.writeText(text);
-    toast.success("CA Copied to clipboard!");
+    toast.success("CA copied to clipboard!");
   } catch (err) {
     console.error("Failed to copy text: ", err);
     toast.error("Failed to copy");
