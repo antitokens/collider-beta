805d24d35106e58959633db55d123b3b89d8c34f
sshmatrix <cryptosauron@protonmail.com>
2025-01-18 21:24:39 +0530

    v0.9.9-lite: Move to main tokens

--- src/components/Inverter.js
+++ src/components/Inverter.js
@@ -439,7 +439,7 @@ const Inverter = ({
                   htmlFor="antiTokens"
                   className="text-accent-secondary font-medium text-xs sm:text-sm"
                 >
-                  ${process.env.NEXT_PUBLIC_TEST_TOKENS ? "t" : ""}PRO
+                  ${process.env.NEXT_PUBLIC_TEST_TOKENS === "true" ? "t" : ""}PRO
                 </label>
                 <span className="border-l border-gray-400/50 h-[0.8rem]"></span>
                 <input
@@ -498,7 +498,7 @@ const Inverter = ({
                   htmlFor="antiTokens"
                   className="text-accent-orange font-medium text-xs sm:text-sm"
                 >
-                  ${process.env.NEXT_PUBLIC_TEST_TOKENS ? "t" : ""}ANTI
+                  ${process.env.NEXT_PUBLIC_TEST_TOKENS === "true" ? "t" : ""}ANTI
                 </label>
               </div>
               <div className={!active ? "hidden" : "text-xs text-gray-500"}>
